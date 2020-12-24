a4b8c6c22290874a05658df24db428af7c844e26
chenxingzhao <1411421720@qq.com>
2020-12-25 00:43:52 +0800

    code end

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,66 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <source-com :source="source" :num="num" @jians="jian" @adds="add">
+    </source-com>
+    <div>
+      男士：{{num.man}} 女士：{{num.woman}} 儿童：{{num.child}}
+    </div>
+    <div>
+      {{zojie}}
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import SourceCom from "./components/SourceCom";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SourceCom
+  },
+  data(){
+    return {
+      source:{
+        man:'N034',
+        woman:'V034',
+        child:'E034',
+        manPrice:'15',
+        womanPrice:'15',
+        childPrice:'7'
+      },
+      num:{
+        man:1,
+        woman:2,
+        child:3
+      },
+      man:['N304'],
+      woman:['V304','V305'],
+      child:['E304','E305','E306']
+    }
+  },
+  methods:{
+    jian(value){
+      if(this.num[value]==1){
+        alert('最低数量是1')
+        return
+      }
+      this.num[value]--;
+      this[value].pop();
+    },
+    add(value){
+      this.num[value]++;
+      let pushs=Number(this[value][0].slice(1));
+      this[value].push(this[value][0][0]+(pushs+this.num[value]));
+    }
+  },
+  mounted() {
+
+  },
+  computed:{
+    zojie(){
+      return this.man.join('、')+'、'+this.woman.join('、')+'、'+this.child.join('、');
+    }
   }
 }
 </script>
---
+++ src/components/SourceCom.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <p>男士：￥{{source.manPrice}} <button @click="jian('man')">-</button>{{num.man}}<button @click="add('man')">+</button></p>
+    <p>女士：￥{{source.womanPrice}} <button @click="jian('woman')">-</button>{{num.woman}}<button @click="add('woman')">+</button></p>
+    <p>儿童：￥{{source.childPrice}} <button @click="jian('child')">-</button>{{num.child}}<button @click="add('child')">+</button></p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "SourceCom",
+    props:{
+      source:{},
+      num:{}
+    },
+    methods:{
+      jian(value){
+        this.$emit('jians',value)
+      },
+      add(value){
+        this.$emit('adds',value)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
